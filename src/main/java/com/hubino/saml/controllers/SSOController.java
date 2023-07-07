b7929d8f615c6d4aa613192eb1a589f100672a6c
Andrii Dziurma <dziurma.talent@gmail.com>
2023-07-07 12:28:12 +0300

    fix: Discovery endpoint response type

--- src/main/java/com/hubino/saml/controllers/SSOController.java
+++ src/main/java/com/hubino/saml/controllers/SSOController.java
@@ -1,8 +1,11 @@
 package com.hubino.saml.controllers;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Set;
 
 @Controller
@@ -26,22 +30,29 @@ public class SSOController {
     private MetadataManager metadata;
 
     @GetMapping("/discovery")
-    public String idpSelection(HttpServletRequest request, Model model) {
+    public ResponseEntity<Object> idpSelection(HttpServletRequest request, Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth == null)
             LOG.debug("Current authentication instance from security context is null");
         else
             LOG.debug("Current authentication instance from security context: {}", this.getClass().getSimpleName());
+        HashMap<String, Object> responseMap = new HashMap<>();
         if (auth == null || (auth instanceof AnonymousAuthenticationToken)) {
             Set<String> idps = metadata.getIDPEntityNames();
             for (String idp : idps)
                 LOG.info("Configured Identity Provider for SSO: {}", idp);
             model.addAttribute("idps", idps);
-            return "pages/discovery";
+            responseMap.put("idps", idps);
+            responseMap.put("message", "DISCOVERY_SUCCESS");
+            responseMap.put("redirect", "pages/discovery");
+            System.out.println(responseMap);
         } else {
             LOG.warn("The current user is already logged.");
-            return "redirect:/landing";
+            responseMap.put("idps", null);
+            responseMap.put("message", "USER_ALREADY_LOGGED");
+            responseMap.put("redirect", "redirect:/landing");
         }
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
     }
 
 }
